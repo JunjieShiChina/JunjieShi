af1a623abfcf8b742796fe6ba4e5d3dc380aa235
shi,Junjie <me.shijunjie@outlook.com>
2018-09-21 22:36:00 +0800

    eureka server

---
+++ src/main/java/me/shijunjie/SimpleEurekaServer/BootStrap.java
@@ -0,0 +1,13 @@
+package me.shijunjie.SimpleEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BootStrap {
+	public static void main(String[] args) {
+		SpringApplication.run(BootStrap.class, args);
+	}
+}
