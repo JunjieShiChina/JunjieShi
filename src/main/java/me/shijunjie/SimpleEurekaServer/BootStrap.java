c2c392aac51d04aeb003840b9d3934364a62ee8f
shi,Junjie <me.shijunjie@outlook.com>
2018-09-23 12:30:16 +0800

    fix

--- src/main/java/me/shijunjie/SimpleEurekaServer/BootStrap.java
+++ src/main/java/me/shijunjie/SimpleEurekaServer/BootStrap.java
@@ -3,11 +3,10 @@ package me.shijunjie.SimpleEurekaServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableEurekaServer
-@PropertySource("classpath:application.yml")
+//@PropertySource("classpath:application.yml")
 public class BootStrap {
 	public static void main(String[] args) {
 		SpringApplication.run(BootStrap.class, args);
